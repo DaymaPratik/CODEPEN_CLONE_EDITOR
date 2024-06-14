65f05d8f9e03e249d3a9bd91c7429dd9ba044c49
Pratik Dayma <pratikdayma45@gamil.com>
2024-06-14 19:10:43 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'react-split-pane': 'react-split-pane',
+    },
+  },
+});
